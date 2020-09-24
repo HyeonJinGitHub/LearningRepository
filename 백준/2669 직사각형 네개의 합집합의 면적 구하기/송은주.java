f45d8d4ce1fc0bb195971f732d8aa16b776fbc2e
OctopusSwellfish <010-2648-8131@daum.net>
2020-09-25 00:27:02 +0900

    2669 직사각형 네개의 합집합의 면적 구하기 - 구현
    일반 배열 좌표처럼 쓰지 않고 좌표계 배열을 사용하더라도 면적은 어쨌든 같은 면적을 차지하기 때문에 a,b포함 c,d미만으로 해서 배열에다가 바꿔주고 그 면적만 계산했당.

---
+++ 백준/2669 직사각형 네개의 합집합의 면적 구하기/송은주.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		boolean[][] arr = new boolean[101][101];
+		for(int i=0; i<4; ++i) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			int d = Integer.parseInt(st.nextToken());
+			
+			for(int k=a; k<c; ++k) {
+				for(int l=b; l<d; ++l) {
+					arr[k][l] = true;
+				}
+			}
+		}
+		
+		int cnt = 0;
+		for(int i=0; i<100; ++i) {
+			for(int j=0; j<100; ++j) {
+				if(arr[i][j]) cnt++;
+			}
+		}
+		
+		System.out.println(cnt);
+	}
+}
