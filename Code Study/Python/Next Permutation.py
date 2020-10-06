68e9549ea5ec1c526963f4c10f84fb94ef5204b6
HyeonJinGitHub <skymilk95@naver.com>
2020-10-07 00:39:51 +0900

    Next Permutation
    Next Permutation 코드입니다.

---
+++ Code Study/Python/Next Permutation.py
@@ -0,0 +1,16 @@
+def next_permutation(a):
+    i = len(a) - 1
+    while i > 0 and a[i-1] >= a[i]:
+        i -= 1
+    if i <= 0:
+        return False
+    j = len(a) - 1
+    while a[i-1] >= a[j]:
+        j -= 1
+    a[i-1], a[j] = a[j], a[i-1]
+    j = len(a) - 1
+    while i < j:
+        a[i], a[j] = a[j], a[i]
+        i += 1
+        j -= 1
+    return True
