573584cb93787fdda3a8b7cb19604285a83a814b
HyeonJinGitHub <skymilk95@naver.com>
2021-01-22 19:46:02 +0900

    2021/01/21 백준 9081 단어 맞추기
    next_permutation으로 해결하였습니다.

---
+++ 백준/9081 단어 맞추기/안현진.py
@@ -0,0 +1,23 @@
+import sys
+
+def next_permutation(a):
+    i = len(a) - 1
+    while i > 0 and a[i - 1] >= a[i]:
+        i -= 1
+    if i <= 0: return False
+    j = len(a) - 1
+    while a[i - 1] >= a[j]:
+        j -= 1
+    a[i - 1], a[j] = a[j], a[i - 1]
+    j = len(a) - 1
+    while i < j:
+        a[i], a[j] = a[j], a[i]
+        i += 1
+        j -= 1
+    return True
+
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        arr = list(map(str, sys.stdin.readline().rstrip()))
+        next_permutation(arr)
+        print(''.join(map(str, arr)))
