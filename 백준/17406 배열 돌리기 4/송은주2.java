ae16efd0b3259e71b9aa5b5bc95e081a05d53987
OctopusSwellfish <010-2648-8131@daum.net>
2021-04-24 01:29:45 +0900

    :sparkles: 배열 돌리기 4
    1112->436 으로 시간 줄어든 코드이다.
    Collections를 쓰지 않고 미리 배열을 할당시켜 준 다음에 인덱스 2개로 조절했더니 시간이 줄었다
    콜렉션이 확실히 엄청나게 시간적으로 부담인듯

---
+++ 백준/17406 배열 돌리기 4/송은주2.java
@@ -0,0 +1,185 @@
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+class Main{
+	private static int N;
+	private static int M;
+	private static int K;
+	private static int[][] input;
+	private static int[][] arr;
+	private static int[] ary;
+	private static boolean[] isSelected;
+	private static int anw;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer
+		st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		input = new int[N][M];
+		arr = new int[K][3];
+		anw = Integer.MAX_VALUE;
+		
+		for(int i=0; i<N; ++i) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for(int j=0; j<M; ++j) {
+				input[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int i=0; i<K; ++i) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for(int j=0; j<3; ++j) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+			arr[i][0]-=1;
+			arr[i][1]-=1;
+		}
+		
+		isSelected = new boolean[K];
+		ary = new int[K];
+		go(0);
+		System.out.print(anw);
+	}
+
+	private static void go(int cnt) {
+		if(cnt==K) {
+//			System.out.println(Arrays.toString(ary));
+			int[][] temp = deepCopy();
+			
+			anw = Math.min(anw, simulate(temp));
+			
+			return;
+		}
+		
+		for(int i=0; i<K; ++i) {
+			if(!isSelected[i]) {
+				isSelected[i] = true;
+				ary[cnt] = i;
+				go(cnt+1);
+				isSelected[i] = false;
+			}
+		}
+		
+	}
+
+	private static int simulate(int[][] temp) {
+		for(int q=0; q<ary.length; ++q) {
+			int x = arr[ary[q]][0];
+			int y = arr[ary[q]][1];
+			int z = arr[ary[q]][2];
+			
+			
+			int n = (x+z) - (x-z);
+			int m = (y+z) - (y-z);
+			
+			
+			int L = Math.min(n, m)/2;
+			for(int l=0; l<L; ++l) {
+//				System.out.println("냐옹");
+				
+				int vert = n - 2*l;
+				int hori = m - 2*l;
+				
+				int a = l;
+				int b = l;
+			
+				int[] o = new int[vert*2+hori*2];
+				int buff = 0;
+				for(int i=0; i<hori; ++i) {
+					o[buff++] = temp[x-z+a][y-z+b];
+					b++;
+				}
+				for(int i=0; i<vert; ++i) {
+					o[buff++] = temp[x-z+a][y-z+b];
+					a++;
+				}
+				for(int i=0; i<hori; ++i) {
+					o[buff++] = temp[x-z+a][y-z+b];
+					b--;
+				}
+				for(int i=0; i<vert; ++i) {
+					o[buff++] = temp[x-z+a][y-z+b];
+					a--;
+				}
+				int idx = o.length-1;
+				//o.offerFirst(o.pollLast());
+				a = b = l;
+				for(int i=0; i<hori; ++i) {
+					temp[x-z+a][y-z+b] = o[idx++];
+					if(idx >= o.length) {
+						idx = 0;
+					}
+					b++;
+				}
+				for(int i=0; i<vert; ++i) {
+					temp[x-z+a][y-z+b] = o[idx++];
+					if(idx >= o.length) {
+						idx = 0;
+					}
+					a++;
+				}
+				for(int i=0; i<hori; ++i) {
+					temp[x-z+a][y-z+b] = o[idx++];
+					if(idx >= o.length) {
+						idx = 0;
+					}
+					b--;
+				}
+				for(int i=0; i<vert; ++i) {
+					temp[x-z+a][y-z+b] = o[idx++];
+					if(idx >= o.length) {
+						idx = 0;
+					}
+					a--;
+				}
+				
+			}
+			
+//			print(temp);
+//			System.out.println("--");
+			
+		}
+		int res = Integer.MAX_VALUE;
+		for(int i=0; i<N; ++i) {
+			int e = 0;
+			for(int j=0; j<M; ++j) {
+				e += temp[i][j];
+			}
+			res = Math.min(e, res);
+		}
+		return res;
+	}
+
+	private static void print(int[][] temp) {
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=0; i<temp.length; ++i) {
+			for(int j=0; j<temp[0].length; ++j) {
+				sb.append(temp[i][j]).append(' ');
+			}
+			sb.append('\n');
+		}
+		System.out.print(sb);
+		
+	}
+
+	private static int[][] deepCopy() {
+		int[][] res = new int[N][M];
+		for(int i=0; i<N; ++i) {
+			for(int j=0; j<M; ++j) {
+				res[i][j] = input[i][j];
+			}
+		}
+		return res;
+	}
+}
