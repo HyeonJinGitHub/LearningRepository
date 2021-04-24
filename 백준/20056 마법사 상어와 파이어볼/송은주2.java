c9896d3c60bab65948a429b8a9c3e03cbe68f096
OctopusSwellfish <010-2648-8131@daum.net>
2021-04-25 01:13:09 +0900

    :sparkles: 마법사 상어와 파이어볼
    구현 문제였는데 세계관(input)이 연결되어서 이상한 문제였다
    어디로 가야하는지가 너무 그림도 없고 모호했던 문제
    시험장에선 잘 풀었던거같은데 이상하다 그땐 그림이 있었던것같은데
    아 그리고 위에서는 rcmds순으로 알려줬으면서 아래에서 입력받을땐 rcmsd순이다 이상함

---
+++ 백준/20056 마법사 상어와 파이어볼/송은주2.java
@@ -0,0 +1,207 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Main{
+	private static int N;
+	private static int M;
+	private static int K;
+	private static int[][] input;
+	private static ArrayList[][] list;
+	private static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
+	private static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
+	private static ArrayList<int[]> fireball;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		input = new int[N][N];
+		list = new ArrayList[N][N];
+		fireball = new ArrayList<int[]>();
+		for(int i=0; i<N; ++i) {
+			for(int j=0; j<N; ++j) {
+				list[i][j] = new ArrayList<Integer>();
+			}
+		}
+		
+		for(int i=0; i<M; ++i) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int r = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			int s = Integer.parseInt(st.nextToken());
+			int d = Integer.parseInt(st.nextToken());
+			fireball.add(new int[] {r-1, c-1, m, d, s});
+//			list[r-1][c-1].add(i);
+		}
+		
+		
+		for(int i=0; i<K; ++i) {
+//			System.out.println("========================"+i);
+			move();
+			bomb();
+//			for(int j=0; j<fireball.size(); ++j) {
+//				System.out.println(Arrays.toString(fireball.get(j)));
+//			}
+		}
+//		System.out.println("---------------------");
+		int anw = 0;
+		for(int i=0; i<fireball.size(); ++i) {
+//			System.out.println(Arrays.toString(fireball.get(i)));
+			anw += fireball.get(i)[2];
+		}
+		System.out.println(anw);
+	}
+
+	private static void bomb() {
+		ArrayList<int[]> movefireball = new ArrayList<>();
+		for(int i=0; i<N; ++i) {
+			for(int j=0; j<N; ++j) {
+				if(list[i][j].size()>0) {
+					if(list[i][j].size()==1) {
+						int[] ball = fireball.get((int)list[i][j].get(0));
+						movefireball.add(ball);
+					}else {
+//						System.out.println("겹쳣음"+i+","+j);
+						int odd = 0;
+						int even = 0;
+						int newM = 0;
+						int newS = 0;
+						for(int k=0; k<list[i][j].size(); ++k) {
+							int[] temp = fireball.get((int)list[i][j].get(k));
+							
+							newM += temp[2];
+							newS += temp[4];
+							
+							if(temp[3]%2==0) {
+								even++;
+							}else odd++;
+						}
+						newM = newM/5;
+						if(newM==0) continue;
+						
+						newS = newS/list[i][j].size();
+						
+//						System.out.println(newM+","+newS+"뉴엠뉴스");
+						if(even==list[i][j].size() || odd==list[i][j].size()) {
+							movefireball.add(new int[] {i, j, newM, 0, newS});
+							movefireball.add(new int[] {i, j, newM, 2, newS});
+							movefireball.add(new int[] {i, j, newM, 4, newS});
+							movefireball.add(new int[] {i, j, newM, 6, newS});
+						}else {
+							movefireball.add(new int[] {i, j, newM, 1, newS});
+							movefireball.add(new int[] {i, j, newM, 3, newS});
+							movefireball.add(new int[] {i, j, newM, 5, newS});
+							movefireball.add(new int[] {i, j, newM, 7, newS});							
+						}
+					}
+				}
+			}
+		}
+		
+//		System.out.println(movefireball.size()+"냐오오옹");
+		fireball = movefireball;
+	}
+
+	private static void move() {
+		ArrayList<int[]> movefireball = new ArrayList<>();
+		
+		clear();
+		for(int i=0; i<fireball.size(); ++i) {
+			
+//			System.out.println(i+"번쨰");
+			int[] temp = fireball.get(i);
+//			System.out.println(Arrays.toString(temp ));
+			int r = temp[0]; int c = temp[1];
+			int m = temp[2]; int d = temp[3]; int s = temp[4];
+//			System.out.println(r+","+c+"시작합니당!");
+			
+			for(int j=0; j<s; ++j) {
+				int nX = r + dx[d];
+				int nY = c + dy[d];
+				
+//				System.out.println(nX+","+nY);
+				if(0<=nX && nX < N && 0<=nY && nY < N) {
+					r = nX;
+					c = nY;
+				}else {
+					
+					if(nX < 0) {
+						r = N-1;
+						if(nY < 0) {
+							 c = N-1;
+						}
+						else if(nY >= N) {
+							c = 0;
+						}else {
+							c = nY;
+						}
+						
+					}
+					else if( nX >= N) {
+						r = 0;
+						if(nY < 0) {
+							 c = N-1;
+						}
+						else if(nY >= N) {
+							c = 0;
+						}else {
+							c = nY;
+						}
+					}
+					
+					if(nY < 0) {
+						 c = N-1;
+						 if(nX < 0) {
+								r = N-1;
+								
+							}
+							else if( nX >= N) {
+								r = 0;
+								
+							}else {
+								r = nX;
+							}
+					}
+					else if(nY >= N) {
+						c = 0;
+						if(nX < 0) {
+							r = N-1;
+							
+						}
+						else if( nX >= N) {
+							r = 0;
+							
+						}else {
+							r= nX;
+						}
+					}
+				}
+//				System.out.println("보정 후 "+r+","+c);
+			}
+			
+//			System.out.println("최종이동후"+r+","+c);
+			
+			movefireball.add(new int[] {r, c, m, d, s});
+			list[r][c].add(i);
+		}
+		fireball = movefireball;
+	}
+
+	private static void clear() {
+		for(int i=0; i<N; ++i) {
+			for(int j=0; j<N; ++j) {
+				list[i][j].clear();
+			}
+		}
+		
+	}
+}
